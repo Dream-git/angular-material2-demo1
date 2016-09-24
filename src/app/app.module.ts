db1f92c340d587d65dbfd74b144aa26f4d64819b
zhang.xiaofei10 <zhang.xiaofei10@zte.com.cn>
2016-09-24 09:21:06 +0800

    init;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {MdCardModule} from '@angular2-material/card';
 import {MdButtonToggleModule} from '@angular2-material/button-toggle';
 import {MdButtonModule} from '@angular2-material/button';
+import {MdSidenavModule} from '@angular2-material/sidenav';
+import {MdToolbarModule} from '@angular2-material/toolbar';
+import {MdIconModule} from '@angular2-material/icon';
+import {MdCheckboxModule} from '@angular2-material/checkbox';
+import {MdRadioModule} from '@angular2-material/radio';
+import {MdInputModule} from '@angular2-material/input';
+import {MdProgressBarModule} from '@angular2-material/progress-bar';
 
 import { AppComponent } from './app.component';
 
@@ -18,7 +26,15 @@ import { AppComponent } from './app.component';
     HttpModule,
     
     MdButtonModule,
-    MdButtonToggleModule
+    MdButtonToggleModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MdInputModule,
+    MdProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
